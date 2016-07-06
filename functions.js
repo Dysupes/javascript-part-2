5d492d2280e4fc35f8ef0373d25e65c8ccf27cec
Dylan Pelletier <dylan.pelletier1979@gmail.com>
2016-07-06 17:42:18 +0000

    Finished the first question

---
+++ functions.js
@@ -0,0 +1,17 @@
+function longestString(str1,str2){
+    if(str1.length < str2.length){
+        return -1
+    } else {
+        return 1
+    }
+};
+
+var sort = longestString;
+
+if (sort = -1){
+    console.log("String 2 is longer!")
+} else {
+    console.log("String 1 is longer!")
+}
+
+longestString("Dylan would love to go back to the Great Wall!","Emma still hasn't visited it yet!");
